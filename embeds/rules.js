266366b58ca403f2ed99f142114ab6af683590dd
Jared Lyon <jaredalyon@gmail.com>
2020-12-01 16:16:47 -0500

    rules n stuff

---
+++ embeds/rules.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'rules',
+    permission: 1,
+    main: function (bot, msg) {
+      msg.channel.send({
+        embed:
+        {
+          color: 3447003,
+          author: {
+            name: "Server Rules",
+            icon_url: msg.guild.iconURL(),
+            footer: "The Democratice National Committee"
+          },
+          fields: [
+            {
+              name: "1. Be respectful.",
+              value: "This rule is fairly self-explainable, and also incredibly easy to apply. Generally, just try to maintain a positive attitude, respect boundaries, and don't be an asshole. In that same regard, speech considered hateful or otherwise unhealthy for the server's atmosphere is heavily subjective; consequently, speech that you find appropriate may not always be appreciated by your peers. With this in mind, please be conscious of how your words might affect those around you."
+            },
+            {
+              name: "2. Use the appropriate text channels.",
+              value: "Since this server is primarily self-governed by those who are on it, I'm not going to always be around to enforce common sense. Don't post anything nsfw, stick the appropriate content in their respective channels, confine bot usage to #bot-console, and use the designated text channels when participating in voice calls."
+            },
+            {
+              name: "3. Do not spread disinformation.",
+              value: "The deliberate dissemination of disinformation can result in immediate removal from the server. Under this rule, dissemination of disinformation includes, but is not limited to, any conduct with intent to convey false or misleading information as fact under circumstances where such information may reasonably be believed."
+            }
+          ]
+        }
+      });
+    }
+  }
